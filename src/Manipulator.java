fcb9b4b71939cdae46873c3acdfcfa112a645148
ZBASH <zlabas2@uic.edu>
2018-11-11 15:32:14 -0600

    removed unsused test/methods

--- src/Manipulator.java
+++ src/Manipulator.java
@@ -2,40 +2,28 @@ public class Manipulator {
     Manipulator() {
     }
 
-    // return an integer from a streing
-    public static int getIntFromString(String s) {
-        return Integer.valueOf(s.replaceAll("\\D+", "").trim());
-    }
-
     // return a reversed integer
     public int reverseInt(int number) {
-
         int arrLength = 1;
-
         int forward = 0;
-
         int reversed = 0;
-
         int counter = 0;
 
         while(forward != number) {
             arrLength *= 10;
             counter++;
             forward = number % arrLength;
-
         }
+
         while(counter > 0) {
             arrLength /= 10;
             reversed += forward % 10 * arrLength;
             forward /= 10;
             counter--;
-        };
-
+        }
         return reversed;
     }
 
     public static void main(String[] args) {
-        String s = "234";
-        System.out.println(getIntFromString(s));
     }
 }
--- src/TestManipulator.java
+++ src/TestManipulator.java
@@ -1,22 +1,11 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestManipulator {
-
-    @org.junit.jupiter.api.Test
-    void getInt() {
-        String s = "sodfjh234";
-        int actual = Integer.valueOf(s.replaceAll("\\D*", ""));
-        int expected = 234;
-        assertEquals(expected,actual);
-    }
-
     @Test
     void flipInt() {
         Manipulator test = new Manipulator();
         int actual = test.reverseInt(54321);
         assertEquals(12345, actual);
     }
-
 }
